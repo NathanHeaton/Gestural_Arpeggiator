bc3ff9bb2936dc2b093c4973caba2fe2fd742cdb
NathanHeaton <nathanmheaton@gmail.com>
2026-04-02 10:48:39 +0100

    got notes to loop inside a for loop, needed to convert 8n to seconds for arthimatic

--- arp.js
+++ arp.js
@@ -1,10 +1,29 @@
 import * as Tone from "tone";
 
+let body = document.getElementById("body");
+
+body?.addEventListener("click", async () => {
+	await Tone.start();
+	console.log("audio is ready");
+
+    Tone.getTransport().start();
+    // ramp up to 800 bpm over 10 seconds
+    Tone.getTransport().bpm.set(120);
+    //playNotes();
+});
+
+let spread = 0;
+
 //create a synth and connect it to the main output (your speakers)
 const synth = new Tone.Synth().toDestination();
 
-//play a middle 'C' for the duration of an 8th note
-synth.triggerAttackRelease("C4", "8n");
+let iterations = 5;
+let noteDuration = Tone.Time("8n").toSeconds();
 
-
-//setInterval(()=> {synth.triggerAttackRelease("C4", "8n")},500)
+const loopA = new Tone.Loop((time) => {
+    let cumlTime = time;
+    for (let i = 0; i < iterations; i++) {
+        synth.triggerAttackRelease("C5", "8t", cumlTime);
+        cumlTime += noteDuration;
+    }
+}, iterations * noteDuration).start(0);
