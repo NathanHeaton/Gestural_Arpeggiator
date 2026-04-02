a6f08cb348647242a099ee4d3bf288828d9dba11
NathanHeaton <nathanmheaton@gmail.com>
2026-04-02 12:25:18 +0100

    added spread and chord slider slider effects note range

--- arp.js
+++ arp.js
@@ -1,6 +1,8 @@
 import * as Tone from "tone";
 
 let body = document.getElementById("body");
+let spreadSlider = document.getElementById("spread");
+let chordSlider = document.getElementById("chord");
 
 body?.addEventListener("click", async () => {
 	await Tone.start();
@@ -12,7 +14,47 @@ body?.addEventListener("click", async () => {
     //playNotes();
 });
 
-let spread = 0;
+
+spreadSlider.oninput = function() {
+    establishNoteArray(Number(this.value), new chord("D", chordTypes.minor))
+}
+
+
+// function updateValues(){
+//     //establishNoteArray(spreadSlider.ariaValueMax,)
+// }
+
+class chord {
+
+    constructor(root, type, octave = 0) {
+        this.root = root;
+        this.type = type;
+        this.octave = octave;
+    }
+
+    getNoteFromBetweenIntervals(start, end){
+        let notes = []
+        console.log(start, end);
+        let degreeOfChord = 0;
+        for (degreeOfChord;degreeOfChord < this.type.length; degreeOfChord++){
+            let rootMidi = Tone.Frequency(`${this.root}${this.octave}`).toMidi()+ this.type[degreeOfChord];
+            let note = rootMidi + Math.ceil((start - rootMidi) / 12) * 12;
+            while (note <= end){
+                if (note >= start){
+                    notes.push(Tone.Frequency(note,"midi").toNote())
+                }
+                note += 12;
+            }
+        }
+        return notes;
+    }
+}
+
+const chordTypes = { major : [0,4,7],
+    minor : [0,3,7],
+    dim : [0,3,6]
+}
+
 
 let arpNotes = ["C4", "E4", "G4", "C5", "D5"];
 
@@ -36,4 +78,9 @@ function selectRandomNote() {
     let i = Math.floor(Math.random()*arpNotes.length);
     console.log(i);
     return arpNotes.at(i);
+}
+
+function establishNoteArray(spread, chord){
+    arpNotes = chord.getNoteFromBetweenIntervals(40,40+spread);
+    console.log(arpNotes);
 }
